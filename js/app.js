b513843f9900d8b1ff32082265e94615ad949706
RyderBlack <ryhad.boughanmi@gmail.com>
2017-01-16 15:04:19 +0100

    upload all 👑

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
-let inputComputer = Math.floor(Math.random() * 100);
-console.log(inputComputer);
-
-const app = angular.module('GuessApp', []);
+const app = angular.module('guessApp', []);
 
     app.controller('MainCtrl', function ($scope) {
 
+      let inputComputer = Math.floor(Math.random() * 100);
+      console.log(inputComputer);
+      $scope.tries = 10;
 
-    $scope.submit = function(inputUser) {
-        if (inputUser === inputComputer) {
-        alert('You win !');
+      $scope.submit = function(inputUser) {
+        if (inputUser < inputComputer) {
+          $scope.indicator = "It's more ! 😉";
+          $scope.tries -= 1;
+          $scope.inputUser = null;
+
+        } else if (inputUser > inputComputer) {
+          $scope.indicator = "It's less ! 😉";
+          $scope.tries -= 1;
+          $scope.inputUser = null;
         } else {
-        alert('You loose !');
-        }
-    };
+          $scope.indicator = "Congratulations ! You guessed the right number !";
+        
+        };
+
+        if ($scope.tries == 0) {
+          $scope.indicator = "Game over";
+          $scope.inputUser = $scope.disabledInput;
+        };
+
+      };
+
 });
